d4de8185075920783787c327f053513a088541bc
ryanholbrook <44940142+ryanholbrook@users.noreply.github.com>
2019-11-06 03:49:06 -0600

    Add external package file for R

---
+++ full/r_packages.R
@@ -0,0 +1,41 @@
+install.packages(
+    # Development
+    "packrat",
+    "archivist",
+    "drake",
+    "testthat",
+    "assertthat",
+    # Utilities,
+    "zeallot",
+    # Data
+    "rio",
+    "roomba",
+    "fst",
+    "disk.frame",
+    "glue",
+    "validate",
+    # EDA
+    "corrplot",
+    "skimr",
+    "lattice",
+    "vcd",
+    "ggforce",
+    "cowplot",
+    "naniar",
+    # Features
+    "vip",
+    # Models
+    "forecast",
+    "kernlab",
+    "naivebayes",
+    "np",
+    "nns",
+    "randomForest",
+    "ranger",
+    "smooth",
+    # Optimization
+    "mlrMBO",
+    # Interpretation
+    "DALEX",
+    "DALEXtra")
+
