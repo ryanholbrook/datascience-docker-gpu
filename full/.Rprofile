31e46f07f78bf437bd9778ffd2b736431bbbc375
ryanholbrook <44940142+ryanholbrook@users.noreply.github.com>
2019-11-20 02:56:56 -0600

    Update :full

--- full/.Rprofile
+++ full/.Rprofile
@@ -1,9 +1,12 @@
-options(max.print=300)
-options(Ncpus=15)
-local({
-    r <- getOption("repos")
-    r["CRAN"] <- "https://cran.revolutionanalytics.com"
-    options(repos = r)
-})
+## Add user profile here
+## For instance:
 
-makeActiveBinding(".", function() .Last.value, env = globalenv())
+## options(max.print=300)
+## options(Ncpus=15)
+## local({
+##     r <- getOption("repos")
+##     r["CRAN"] <- "https://cran.revolutionanalytics.com"
+##     options(repos = r)
+## })
+
+## makeActiveBinding(".", function() .Last.value, env = globalenv())
--- full/r_packages.R
+++ full/r_packages.R
@@ -1,20 +1,23 @@
 install.packages(
-    # Development
+    ## Development
     "packrat",
     "archivist",
     "drake",
     "testthat",
     "assertthat",
-    # Utilities,
+
+    ## Utilities,
     "zeallot",
-    # Data
+
+    ## Data
     "rio",
     "roomba",
     "fst",
     "disk.frame",
     "glue",
     "validate",
-    # EDA
+
+    ## EDA
     "corrplot",
     "skimr",
     "lattice",
@@ -22,41 +25,102 @@ install.packages(
     "ggforce",
     "cowplot",
     "naniar",
-    # Features
+
+    ## Features
     "vip",
-    # Models
+    "FactoMineR",
+    "Boruta",
+
+    ## Models
+    ## ------
+    ## Linear Regression
+    "brms", # MCMC Bayesian
+    "glmnet", # lasso
+    "elasticnet", # L_p regularization
+    "pls", # partial least squares and principal component regression
+
+    ## Linear Classification
+    "DiscriMiner", # discriminant analysis
+    "MASS", # lda, mca, qda
+    "mda", # Mixture DA
+    "sda" # Shrinkage DA
+
+    ## Basis Expansion
+    ## Splines, Kernel Smoothing, and Local Regression
+    "crs", # Categorical Regression Splines
+    "earth", # Multivariate Adaptive Regression Splines
+    "KernelKnn",
+    "kernlab",
+    "naivebayes",
+    "nns", # nonparametric models using partial moments
+    "np", # nonparametric kernel methods
+    "polyspline", # Polynomial MARS
+
+    ## Model Averaging
     "adabag",
-    "brms",
+    "ipred",
+
+    ## Additive Models and Trees
+    "gam",
+    "party", "partykit",
+    "rpart",
+
+    ## Boosting and Additive Trees
+    "ada",
+    "bst",
+    "gbm"
+    "caTools", # logicboost
+    "mboost", # glmboost and gamboost
     "C50",
     "Cubist",
-    "DiceKriging",
-    "e1071",
-    "earth",
-    "elasticnet",
-    "forecast",
-    "gbm"
-    "glmnet",
-    "kernlab",
-    "kknn",
-    "klaR",
-    "laGP",
-    "MASS",
-    "mboost",
-    "mda",
-    "naivebayes",
-    "np",
-    "nns",
+
+    ## Neural Networks and Projection Pursuit
+    "deepnet",
     "nnet",
-    "party",
-    "pls"
+
+    ## Support Vector Machines
+    "e1071",
+    ## kernlab
+    ## liquid svm
+    ## swarm svm
+
+    ## Prototype Methods and Nearest-Neighbors
+    "kknn",
+    "class",    
+    "OneR",
+
+    ## Random Forests
     "randomForest",
     "ranger",
     "Rborist",
-    "rpart",
-    "sda",
+    "rFerns",
+
+    ## Ensembles
+
+    ## Gaussian Processes
+    "DiceKriging",
+    "laGP",
+
+    ## Time Series
+    "forecast",
+    "prophet",
     "smooth",
-    # Optimization
+
+    ## Clustering
+    "cluster",
+    "clue",
+    "fpc",
+    "klaR",
+    "mclust",
+
+    ## Rules
+    "arules",
+    ## RcppCorels 
+
+    # Optimization and Design
     "mlrMBO",
+    "rsm",
+    
     # Interpretation
     "DALEX",
     "DALEXtra")
