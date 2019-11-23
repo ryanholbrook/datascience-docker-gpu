b6cc713b920c4552864fe7b980abb815508fb269
ryanholbrook <44940142+ryanholbrook@users.noreply.github.com>
2019-11-23 07:10:36 -0600

    Pass keyword and positional arguments separately

--- full/datasci.sh
+++ full/datasci.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
+# launch the containter with X support
 # See https://skandhurkat.com/post/x-forwarding-on-docker/
 
-XAUTH=$HOME/.Xauthority
-touch $XAUTH
+# separate keyword and positional arguments
+KEYWORD=()
+POSITIONAL=()
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --*|-*)
+            KEYWORD+=("${1} ${2}")
+            shift 2
+            ;;
+        *)
+            POSITIONAL+=("${1}")
+            shift
+            ;;
+    esac
+done
 
 docker run \
        --name datascience \
@@ -12,7 +26,8 @@ docker run \
        --gpus all \
        --network=host\
        --env DISPLAY=$DISPLAY \
-       --volume $XAUTH:/home/jovyan/.Xauthority \
-       
+       --volume $XAUTHORITY:/home/jovyan/.Xauthority \
+       $KEYWORD \
        rholbrook/datascience:full \
-       start.sh "$@"
+       start.sh \
+       ${POSITIONAL}
