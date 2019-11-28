4d89d56de5c7660a42f2d577ca67e2c99fb04379
ryanholbrook <44940142+ryanholbrook@users.noreply.github.com>
2019-11-28 03:33:00 -0600

    Cleanup packages

--- full/install.R
+++ full/install.R
@@ -1,14 +1,11 @@
-install.packages(
+packages <- (
     ## Development
-    "packrat",
     "archivist",
     "drake",
     "testthat",
     "assertthat",
-
     ## Utilities,
     "zeallot",
-
     ## Data
     "rio",
     "roomba",
@@ -16,113 +13,127 @@ install.packages(
     "disk.frame",
     "glue",
     "validate",
-
     ## EDA
     "corrplot",
     "skimr",
     "lattice",
     "vcd",
     "ggforce",
-    "cowplot",
+    "gganimate",
+    "patchwork",
     "naniar",
-
     ## Features
     "vip",
     "FactoMineR",
     "Boruta",
-
     ## Models
-    ## ------
+    ## --------------------
     ## Linear Regression
     "brms", # MCMC Bayesian
     "glmnet", # lasso
     "elasticnet", # L_p regularization
     "pls", # partial least squares and principal component regression
-
-    ## Linear Classification
-    "DiscriMiner", # discriminant analysis
+    ## Disciminant Analysis
+    ## --------------------
+    "DiscriMiner",
     "MASS", # lda, mca, qda
     "mda", # Mixture DA
     "sda" # Shrinkage DA
-
-    ## Basis Expansion
+    "naivebayes",## Basis Expansion
     ## Splines, Kernel Smoothing, and Local Regression
+    ## --------------------
     "crs", # Categorical Regression Splines
     "earth", # Multivariate Adaptive Regression Splines
     "KernelKnn",
     "kernlab",
-    "naivebayes",
     "nns", # nonparametric models using partial moments
     "np", # nonparametric kernel methods
     "polyspline", # Polynomial MARS
-
     ## Model Averaging
+    ## --------------------
     "adabag",
     "ipred",
-
     ## Additive Models and Trees
+    ## --------------------
     "gam",
     "party", "partykit",
     "rpart",
-
-    ## Boosting and Additive Trees
+    "C50",
+    "Cubist",
+    ## Boosting
+    ## --------------------
     "ada",
     "bst",
     "gbm"
     "caTools", # logicboost
     "mboost", # glmboost and gamboost
-    "C50",
-    "Cubist",
-
     ## Neural Networks and Projection Pursuit
-    "deepnet",
+    ## --------------------
     "nnet",
-
+    "deepnet",
     ## Support Vector Machines
+    ## --------------------
     "e1071",
+    "liquidSVM",
     ## kernlab
     ## liquid svm
     ## swarm svm
-
     ## Prototype Methods and Nearest-Neighbors
+    ## --------------------
     "kknn",
     "class",    
     "OneR",
-
     ## Random Forests
+    ## --------------------
     "randomForest",
     "ranger",
     "Rborist",
     "rFerns",
-
     ## Ensembles
-
+    ## --------------------
     ## Gaussian Processes
+    ## --------------------
     "DiceKriging",
     "laGP",
-
     ## Time Series
+    ## --------------------
     "forecast",
     "prophet",
     "smooth",
-
     ## Clustering
+    ## --------------------
     "cluster",
     "clue",
     "fpc",
     "klaR",
     "mclust",
-
     ## Rules
+    ## --------------------
     "arules",
-    ## RcppCorels 
-
-    # Optimization and Design
+    ## RcppCorels
+    ## --------------------
+    ## Optimization and Design
+    ## --------------------
     "mlrMBO",
-    "rsm",
-    
-    # Interpretation
+    ## Interpretation
+    ## --------------------
     "DALEX",
     "DALEXtra")
 
-install.packages("liquidSVM", configure.args = "native /usr/local/cuda")
+## Not compatible with CUDA 10
+## install.packages("liquidSVM", configure.args = "native /usr/local/cuda")
+
+not_installed <- function(pkg) {
+    !nzchar(system.file(package = pkg))
+}
+
+install_if_not <- function(pkgs) {
+    sapply(pkgs, 
+           function (pkg)
+               if (not_installed(pkg))
+                   install.packages(pkg)
+           )
+    return(NULL)
+}
+
+install_if_not(packages)
